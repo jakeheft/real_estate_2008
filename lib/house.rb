4ced591139799a1b487dfbedaab2d529fc4a69da
Jake Heft <jakeheft@gmail.com>
2020-08-26 09:37:10 -0600

    Add test can see rooms in house by category

--- lib/house.rb
+++ lib/house.rb
@@ -12,6 +12,12 @@ class House
   end
 
   def above_market_average?
-    @price > 500000    
+    @price > 500000
+  end
+
+  def rooms_from_category(category)
+    @rooms.select do |room|
+      room.category == category
+    end
   end
 end
